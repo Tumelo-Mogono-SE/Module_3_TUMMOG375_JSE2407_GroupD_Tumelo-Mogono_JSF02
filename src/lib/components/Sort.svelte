f514b657d12e55ffe0c91140b4d3ba86a41b5d3f
Tumelo Mogono <tumelomogono@gmail.com>
2024-07-30 16:33:40 +0200

    Created my sort, which takes store and subscribes to pass value of sort and update sort products. Also added options to select sort option.

---
+++ src/lib/components/Sort.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { onMount } from 'svelte';
+    import { productStore } from '../../store/store.js';
+
+    let sorting;
+
+    const { subscribe, sortProducts, setSorting } = productStore;
+
+    onMount(() => {
+        subscribe(value => {
+        sorting = value.sorting;
+        });
+    });
+
+    const handleSort = (event) => {
+        setSorting(event.target.value);
+        sortProducts();
+    };
+</script>
+
+
+<div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+    <label for="sort" class="w-20 my-auto font-semibold">
+      Sort by: 
+    </label>
+    <select
+      on:change={handleSort}
+      bind:value={sorting}
+      name=""
+      id="sort"
+      class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+    >
+      <option value="default">Default</option>
+      <option value="low">Price: Low to High</option>
+      <option value="high">Price: High to Low</option>
+    </select>
+  </div>
+  
