f7b2d50514afb0729d9155574b1f5b7cca22f9dd
Tumelo Mogono <tumelomogono@gmail.com>
2024-07-30 16:57:13 +0200

    Imported router and productdetailview page which I use by creating routes to home and productdtailview.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+  import Router  from 'svelte-spa-router';
   import NavBar from './lib/components/NavBar.svelte';
   import Home from './lib/pages/Home.svelte';
+  import ProductDetailView from './lib/pages/ProductDetailView.svelte';
+
+  const routes = {
+    '/': Home,
+    '/products/:id': ProductDetailView,
+  };
 </script>
 
 
 <NavBar />
 <main>
-  <Home />
+  <Router {routes} />
 </main>
