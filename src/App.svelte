79453030ccea83de8490b7183c139ecd0b6814f7
Tumelo Mogono <tumelomogono@gmail.com>
2024-07-30 16:35:31 +0200

    imported Home to be rendered.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
-  import NavBar from './lib/components/NavBar.svelte'; 
+  import NavBar from './lib/components/NavBar.svelte';
+  import Home from './lib/pages/Home.svelte';
 </script>
 
 
 <NavBar />
+<main>
+  <Home />
+</main>
