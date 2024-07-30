e9b23419a6f8c6b4a5b21953cd930626ba5aaf58
Tumelo Mogono <tumelomogono@gmail.com>
2024-07-30 16:35:19 +0200

    Imported filter , sort and productList to make the home page.

---
+++ src/lib/pages/Home.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Filter from '../components/Filter.svelte';
+    import Sort from '../components/Sort.svelte';
+    import ProductList from '../components/ProductList.svelte';
+</script>
+
+
+<div class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center">
+    <Filter />
+    <Sort />
+</div>
+<ProductList />
